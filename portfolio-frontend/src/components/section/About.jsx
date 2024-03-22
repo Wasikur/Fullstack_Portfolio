cf0e9f9cc082b7c19b8de27010495a8e4b7d6c5e
Wasikur <69371416+Wasikur@users.noreply.github.com>
2024-03-22 16:55:44 +0530

    Update About.jsx

--- portfolio-frontend/src/components/section/About.jsx
+++ portfolio-frontend/src/components/section/About.jsx
@@ -5,61 +5,56 @@ import AboutCard from "./AboutTile/AboutCard";
 import about from "./AboutTile/aboutCardDetails";
 
 const About = () => {
-  const navigateToExperience = () => {
-    window.location.href = "./#experience";
-  };
+const navigateToExperience = () => {
+window.location.href = "./#experience";
+};
 
-  function createCard(about) {
-    return (
-      <AboutCard
-        id={about.id}
-        key={about.id}
-        icon={about.icon}
-        title={about.title}
-        details1={about.details1}
-        details2={about.details2}
-        details3={about.details3}
-      />
-    );
-  }
+function createCard(about) {
+return (
+<AboutCard id={about.id} key={about.id} icon={about.icon} title={about.title} details1={about.details1}
+    details2={about.details2} details3={about.details3} />
+);
+}
 
-  return (
-    <section id="about">
-      <p className="section__text__p1">Get To Know More</p>
-      <h1 className="title">About Me</h1>
-      <div className="section-container">
+return (
+<section id="about">
+    <p className="section__text__p1">Get To Know More</p>
+    <h1 className="title">About Me</h1>
+    <div className="section-container">
         <div className="section__pic-container">
-          <img src={about_me2} alt="Portrait" className="about-pic" />
+            <img src={about_me2} alt="Portrait" className="about-pic" />
         </div>
         <div className="about-details-container">
-          <div className="about-containers">{about.map(createCard)}</div>
-          <div className="text-container">
-            <p>
-              Enthusiastic FullStack Web Developer | MERN Stack Expert | ECE Fresher with Strong Python & Database Skills
-            </p>
-               <p>
-             As a passionate and highly motivated FullStack Web Developer, I bring a unique blend of expertise in both web development and Electronics and Communication Engineering (ECE). Proficient in MERN stack technologies, including MongoDB, Express.js, React.js, and Node.js, I am eager to apply my skills in real-world projects.
-            </p>
-                  <p>
-             With a solid foundation in Python programming and database management, acquired through internships at Doordarshan Kendra, Airport Authority of India, and Oil India Limited, I have honed my abilities in data manipulation, visualization, and analysis. Additionally, I excel in MySQL for database management and advanced features of MS Excel for data cleansing and transformation.
-            </p>
-                  <p>
-             I thrive on solving complex problems and identifying opportunities for innovation. My analytical mindset, attention to detail, and commitment to continuous learning drive me to deliver high-quality solutions. I am excited to leverage my technical skills and passion for web development to contribute effectively to a dynamic team.
-            </p>
-                  <p>
-             In summary, as a fresher ECE Engineer with a focus on FullStack web development and proficiency in MERN stack technologies, Python, and database management, I am prepared to make valuable contributions to innovative projects and drive business success.
-            </p>
-          </div>
+            <div className="about-containers">{about.map(createCard)}</div>
+            <div className="text-container">
+                <p>
+                    Enthusiastic FullStack Web Developer | MERN Stack Expert | ECE Fresher with Strong Python & Database
+                    Skills
+                </p>
+              </br>
+                <p>
+                    As a passionate and highly motivated FullStack Web Developer, I bring a unique blend of expertise in
+                    both web development and Electronics and Communication Engineering (ECE). Proficient in MERN stack
+                    technologies, including MongoDB, Express.js, React.js, and Node.js, I am eager to apply my skills in
+                    real-world projects.
+                </p>
+                <p>
+                    I thrive on solving complex problems and identifying opportunities for innovation. My analytical
+                    mindset, attention to detail, and commitment to continuous learning drive me to deliver high-quality
+                    solutions. I am excited to leverage my technical skills and passion for web development to
+                    contribute effectively to a dynamic team.
+                </p>
+                <p>
+                    In summary, as a fresher ECE Engineer with a focus on FullStack web development and proficiency in
+                    MERN stack technologies, Python, and database management, I am prepared to make valuable
+                    contributions to innovative projects and drive business success.
+                </p>
+            </div>
         </div>
-      </div>
-      <img
-        src={arrow}
-        alt="Arrow icon"
-        className="icon arrow"
-        onClick={navigateToExperience}
-      />
-    </section>
-  );
+    </div>
+    <img src={arrow} alt="Arrow icon" className="icon arrow" onClick={navigateToExperience} />
+</section>
+);
 };
 
 export default About;
