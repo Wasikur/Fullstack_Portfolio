0fb73f338499e70d38ca2b34a95334878aeafdbb
Wasikur <69371416+Wasikur@users.noreply.github.com>
2024-03-22 14:56:51 +0530

    Update insert_doc.js

--- portfolio-backend/insert_doc.js
+++ portfolio-backend/insert_doc.js
@@ -4,26 +4,26 @@ const createDocument = async () => {
     const pj1 = new Projectmodel({
       key: 1,
       img: "../assets/project1.jpg",
-      alt: "project1",
-      title: "Project One",
-      githubLink: "",
-      demoLink:"",
+      alt: "my_portfolio_project",
+      title: "Portfolio Project",
+      githubLink: "https://github.com/Wasikur/Fullstack_Portfolio.git",
+      demoLink:"https://fullstack-portfolio-frontend.vercel.app/",
     });
     const pj2 = new Projectmodel({
       key: 2,
       img: "../assets/project2.jpg",
-      alt: "project2",
-      title: "Project Two",
-      githubLink: "",
-      demoLink:"",
+      alt: "fullstack_service_website",
+      title: "Service Website Project with Admin Panel",
+      githubLink: "https://github.com/Wasikur/fullstack-service.git",
+      demoLink:"https://fullstack-service-client.vercel.app/",
     });
     const pj3 = new Projectmodel({
       key: 3,
       img: "../assets/project3.jpg",
-      alt: "project3",
-      title: "Project Three",
-      githubLink: "",
-      demoLink:"",
+      alt: "swm-project",
+      title: "Solid Waste Management Plant Project",
+      githubLink: "https://github.com/Wasikur/SWM_reactJS.git",
+      demoLink:"https://swm-react-js.vercel.app/",
     });
 
     const result = await Projectmodel.insertMany([pj1, pj2, pj3]);
